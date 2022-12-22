89cde9a9554117c03957368a00eb182a9f3700f5
LeviTheSkriptKiddie <102006486+LeviTheSkriptKiddie@users.noreply.github.com>
2022-12-22 04:59:37 -0800

    Uploading Files From Ep 1
    Same Exact Files

---
+++ YT RAT CLIENT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+// Create new TCP/IP socket for remote access
+IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+
+// Connect To the Remote Access Server
+socket.Connect(iPEndPoint);
+
+// Send Command To server
+string command = "Connected To are first RAT server!";
+byte[] commandBuffer = Encoding.ASCII.GetBytes(command);
+socket.Send(commandBuffer);
+
+// Receive Response From Server
+byte[] responseBuffer = new byte[1024];
+int recivedBytes = socket.Receive(responseBuffer);
+string response = Encoding.ASCII.GetString(responseBuffer, 0, recivedBytes);
+
+// Diplay The Response
+Console.WriteLine("response");
+Console.ReadLine();
+
+//Close The Socket
+socket.Close();
---
+++ YT RAT/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+// Create New TCP/IP socket for Remote Access
+IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 8080);
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+socket.Bind(iPEndPoint);
+
+// Listen for connection from Clients...
+
+socket.Listen(10);
+Socket clientSocket = socket.Accept();
+
+// Recive data from the remote user
+
+byte[] buffer = new byte[1024];
+int recivedBytes = clientSocket.Receive(buffer);
+
+// Process the recived data
+string recivedData = Encoding.ASCII.GetString(buffer, 0,recivedBytes);
+
+// Execute the recived command 
+Console.WriteLine(recivedData);
+
+//Send A response to the remote user
+string response = "command Successfully Executed";
+byte[] ResponseBuffer = Encoding.ASCII.GetBytes(response);
+clientSocket.Send(ResponseBuffer);
+Console.ReadLine();
+
+// Close The server off
+socket.Close();
+
+
